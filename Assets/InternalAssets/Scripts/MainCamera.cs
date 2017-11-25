7601741ecd5f0d7a48f6c3413fddc4965e3a751f
Xenation <xenations.111@gmail.com>
2017-11-25 10:49:49 +0100

    Split Screen creation on play

--- Assets/InternalAssets/Scripts/MainCamera.cs
+++ Assets/InternalAssets/Scripts/MainCamera.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainCamera : MonoBehaviour {
 
@@ -6,6 +7,8 @@ public class MainCamera : MonoBehaviour {
 	public PlayerCamera camera2;
 	public PlayerCamera camera3;
 	public PlayerCamera camera4;
+
+	public RectTransform splitScreensRoot;
 
 	private int numberOfPlayers;
 
@@ -35,6 +38,30 @@ public class MainCamera : MonoBehaviour {
 		camera2.Init(pcamWidth, pcamHeight);
 		camera3.Init(pcamWidth, pcamHeight);
 		camera4.Init(pcamWidth, pcamHeight);
+		CreateUIScreens();
+	}
+
+	private void CreateUIScreens() {
+		CreateUIScreen("Screen1", 0f, .5f, .5f, 1f, camera1.targetMat);
+		CreateUIScreen("Screen2", .5f, 1f, .5f, 1f, camera2.targetMat);
+		CreateUIScreen("Screen3", 0f, .5f, 0f, .5f, camera3.targetMat);
+		CreateUIScreen("Screen4", .5f, 1f, 0f, .5f, camera4.targetMat);
+	}
+
+	private GameObject CreateUIScreen(string name, float xmin, float xmax, float ymin, float ymax, Material mat) {
+		GameObject go = new GameObject();
+		go.transform.SetParent(splitScreensRoot);
+		go.name = name;
+		RectTransform rect = go.AddComponent<RectTransform>();
+		rect.anchorMin = new Vector2(xmin, ymin);
+		rect.anchorMax = new Vector2(xmax, ymax);
+		float width = xmax - xmin;
+		float height = ymax - ymin;
+		rect.offsetMin = Vector2.zero;
+		rect.offsetMax = Vector2.zero;
+		RawImage img = go.AddComponent<RawImage>();
+		img.material = mat;
+		return go;
 	}
 
 }
