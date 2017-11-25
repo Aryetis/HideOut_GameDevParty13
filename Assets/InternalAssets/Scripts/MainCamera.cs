b096e009f29386486151c315542f9477799afd4f
Xenation <xenations.111@gmail.com>
2017-11-25 11:35:16 +0100

    adaptive screen count

--- Assets/InternalAssets/Scripts/MainCamera.cs
+++ Assets/InternalAssets/Scripts/MainCamera.cs
@@ -3,49 +3,42 @@ using UnityEngine.UI;
 
 public class MainCamera : MonoBehaviour {
 
-	public PlayerCamera camera1;
-	public PlayerCamera camera2;
-	public PlayerCamera camera3;
-	public PlayerCamera camera4;
+	public PlayerCamera[] cameras;
+	public int countX = 2;
+	public int countY = 2;
 
 	public RectTransform splitScreensRoot;
 
-	private int numberOfPlayers;
+
+	public int NumberOfPlayers {
+		get {
+			return cameras.Length;
+		}
+	}
 
 	private Camera cam;
 
 	private void Awake() {
 		cam = GetComponent<Camera>();
-		numberOfPlayers = 0;
-		if (camera1 != null) {
-			numberOfPlayers++;
-		}
-		if (camera2 != null) {
-			numberOfPlayers++;
-		}
-		if (camera3 != null) {
-			numberOfPlayers++;
-		}
-		if (camera4 != null) {
-			numberOfPlayers++;
-		}
 	}
 
 	private void Start() {
 		int pcamWidth = cam.pixelWidth / 2;
 		int pcamHeight = cam.pixelHeight / 2;
-		camera1.Init(pcamWidth, pcamHeight);
-		camera2.Init(pcamWidth, pcamHeight);
-		camera3.Init(pcamWidth, pcamHeight);
-		camera4.Init(pcamWidth, pcamHeight);
+		foreach (PlayerCamera pcam in cameras) {
+			pcam.Init(pcamWidth, pcamHeight);
+		}
 		CreateUIScreens();
 	}
 
 	private void CreateUIScreens() {
-		CreateUIScreen("Screen1", 0f, .5f, .5f, 1f, camera1.targetMat);
-		CreateUIScreen("Screen2", .5f, 1f, .5f, 1f, camera2.targetMat);
-		CreateUIScreen("Screen3", 0f, .5f, 0f, .5f, camera3.targetMat);
-		CreateUIScreen("Screen4", .5f, 1f, 0f, .5f, camera4.targetMat);
+		float width = 1f / countX;
+		float height = 1f / countY;
+		for (int i = 0; i < cameras.Length; i++) {
+			float posX = ((countX != 1) ? i % countX : 0) * width;
+			float posY = 1 - ((countY != 1) ? i / countY : 0) * width;
+			CreateUIScreen("Screen" + i, posX, posX + width, posY - height, posY, cameras[i].targetMat);
+		}
 	}
 
 	private GameObject CreateUIScreen(string name, float xmin, float xmax, float ymin, float ymax, Material mat) {
