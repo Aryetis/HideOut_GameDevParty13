e9fb58a3377813f583abbadbfc24338f998ffa7c
HijackedBrain <aramirg@hotmail.fr>
2017-11-25 12:10:45 +0100

    First draft for controllers mixing and player selection scene

---
+++ Assets/InternalAssets/Scripts/JoystickManager.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JoystickManager : MonoBehaviour
+{
+    private List<GameObject> playersList;
+    private GameObject playersFolder;
+    private List<GameObject> statusList;
+    private bool[] joystickStatus; // false <=> unattributed joystick
+    private int activePlayersNumber;
+    private float TimerToStart;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playersList = new List<GameObject>();
+        playersFolder = GameObject.Find("PlayersFolder");
+
+        statusList = new List<GameObject>();
+        statusList.Add(GameObject.Find("Player1Status"));
+        statusList.Add(GameObject.Find("Player2Status"));
+        statusList.Add(GameObject.Find("Player3Status"));
+        statusList.Add(GameObject.Find("Player4Status"));
+
+        foreach(GameObject go in statusList)
+            go.SetActive(false);
+
+        joystickStatus = new bool[] {false, false, false, false, false, false, false, false};
+
+        foreach(Transform t in playersFolder.transform)
+        {
+            playersList.Add(t.gameObject);
+            t.gameObject.SetActive(false);
+        }
+
+        activePlayersNumber = 0;
+        TimerToStart = 2.0f;
+	}
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        /********************************************
+         * ASSIGNING JOYSYICK NUMBER TO EACH PLAYER *
+         ********************************************/
+        if(Input.GetButton("PALL_Start") && activePlayersNumber <= 4)
+        {
+            if(Input.GetKeyDown(KeyCode.Joystick1Button7) && !joystickStatus[0] )
+            {
+                Debug.Log("KeyCode.Joystick1Button7 START PRESSED");
+                statusList[activePlayersNumber].SetActive(true);
+                playersList[activePlayersNumber].SetActive(true);
+                playersList[activePlayersNumber].GetComponent<PlayerInputs>().joyNumber = 0;
+                activePlayersNumber++;
+                joystickStatus[0] = true; 
+            }
+            if(Input.GetKeyDown(KeyCode.Joystick2Button7) && !joystickStatus[1] )
+            {
+                Debug.Log("KeyCode.Joystick1Button7 START PRESSED");
+                statusList[activePlayersNumber].SetActive(true);
+                playersList[activePlayersNumber].SetActive(true);
+                playersList[activePlayersNumber].GetComponent<PlayerInputs>().joyNumber = 1;
+                activePlayersNumber++;
+                joystickStatus[1] = true;
+            }
+            if(Input.GetKeyDown(KeyCode.Joystick3Button7) && !joystickStatus[2] )
+            {
+                Debug.Log("KeyCode.Joystick1Button7 START PRESSED");
+                statusList[activePlayersNumber].SetActive(true);
+                playersList[activePlayersNumber].SetActive(true);
+                playersList[activePlayersNumber].GetComponent<PlayerInputs>().joyNumber = 2;
+                activePlayersNumber++;
+                joystickStatus[2] = true; 
+            }
+            if(Input.GetKeyDown(KeyCode.Joystick4Button7) && !joystickStatus[3] )
+            {
+                Debug.Log("KeyCode.Joystick4Button7 START PRESSED");
+                statusList[activePlayersNumber].SetActive(true);
+                playersList[activePlayersNumber].SetActive(true);
+                playersList[activePlayersNumber].GetComponent<PlayerInputs>().joyNumber = 3;
+                activePlayersNumber++;
+                joystickStatus[3] = true;   
+            }
+            if(Input.GetKeyDown(KeyCode.Joystick5Button7) && !joystickStatus[4] )
+            {                
+                Debug.Log("KeyCode.Joystick5Button7 START PRESSED");
+                statusList[activePlayersNumber].SetActive(true);
+                playersList[activePlayersNumber].SetActive(true);
+                playersList[activePlayersNumber].GetComponent<PlayerInputs>().joyNumber = 4;
+                activePlayersNumber++;
+                joystickStatus[4] = true;    
+            }
+            if(Input.GetKeyDown(KeyCode.Joystick6Button7) && !joystickStatus[5] )
+            {
+                Debug.Log("KeyCode.Joystick6Button7 START PRESSED");
+                statusList[activePlayersNumber].SetActive(true);
+                playersList[activePlayersNumber].SetActive(true);
+                playersList[activePlayersNumber].GetComponent<PlayerInputs>().joyNumber = 5;
+                activePlayersNumber++;
+                joystickStatus[5] = true; 
+            }
+            if(Input.GetKeyDown(KeyCode.Joystick7Button7) && !joystickStatus[6] )
+            {
+                Debug.Log("KeyCode.Joystick7Button7 START PRESSED");
+                statusList[activePlayersNumber].SetActive(true);
+                playersList[activePlayersNumber].SetActive(true);
+                playersList[activePlayersNumber].GetComponent<PlayerInputs>().joyNumber = 6;
+                activePlayersNumber++;
+                joystickStatus[6] = true; 
+            }
+            if(Input.GetKeyDown(KeyCode.Joystick8Button7) && !joystickStatus[7] )
+            {
+                Debug.Log("KeyCode.Joystick8Button7 START PRESSED");
+                statusList[activePlayersNumber].SetActive(true);
+                playersList[activePlayersNumber].SetActive(true);
+                playersList[activePlayersNumber].GetComponent<PlayerInputs>().joyNumber = 7;
+                activePlayersNumber++;
+                joystickStatus[7] = true; 
+            }
+
+            TimerToStart -= Time.deltaTime;
+        }
+
+        /*****************************
+         * HOLD START TO LAUNCH GAME *
+         *****************************/
+        if(TimerToStart <= 0)
+            Debug.Log("LAUNCHING GAME");
+	}
+}
--- Assets/InternalAssets/Scripts/PlayerInputs.cs
+++ Assets/InternalAssets/Scripts/PlayerInputs.cs
@@ -2,15 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerInputs : MonoBehaviour {
+public class PlayerInputs : MonoBehaviour 
+{
+    [Range(1,4)] public int joyNumber; // use Kecode.KeyCode.Joystick[joyNumber+1]ButtonXXX for buttons , and P[joyNumber]_[Horizontal/Vertical] for axis
+    public float speed;
+
+    private float leftJoystickHorizontalInput;
+    private float leftJoystickVerticalInput;
+    private float rightJoystickHorizontalInput;
+    private float rightJoystickVerticalInput;
+    private bool buttonA;
+    private bool buttonB;
+    private bool buttonX;
+    private bool buttonY;
+    private bool lb;
+    private bool rb;
+    private CharacterController cc;
+    private Vector3 moveVector;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        moveVector = new Vector3();
+        cc = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        /*******************************************
+         * Update inputs according to playerNumber *
+         *******************************************/
+        switch(joyNumber)
+        {
+            case 0:
+            {
+                leftJoystickHorizontalInput = Input.GetAxis("P1_Horizontal");
+                leftJoystickVerticalInput = Input.GetAxis("P1_Vertical");
+                break;
+            }
+            case 1:
+            {
+                leftJoystickHorizontalInput = Input.GetAxis("P2_Horizontal");
+                leftJoystickVerticalInput = Input.GetAxis("P2_Vertical");
+                break;
+            }
+            case 2:
+            {
+                leftJoystickHorizontalInput = Input.GetAxis("P3_Horizontal");
+                leftJoystickVerticalInput = Input.GetAxis("P3_Vertical");
+                break;
+            }
+            case 3:
+            {
+                leftJoystickHorizontalInput = Input.GetAxis("P4_Horizontal");
+                leftJoystickVerticalInput = Input.GetAxis("P4_Vertical");
+                break;
+            }
+            default: { /*Debug.LogError("playerNumber not set for a player");*/ break;}
+        }
+
+        /************************************
+         *        APPLY MOVE VECTOR         *
+         ************************************/
+        moveVector.x = leftJoystickHorizontalInput;
+        moveVector.z = leftJoystickVerticalInput;
+        cc.Move(moveVector * speed * Time.deltaTime);
 	}
 }
