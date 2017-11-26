ceaeedd94b859ede0d4661ab0456122e17bdf8e5
HijackedBrain <aramirg@hotmail.fr>
2017-11-26 10:57:24 +0100

    initial menus

---
+++ Assets/InternalAssets/Scripts/MainMenuBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuBehavior : MonoBehaviour
+{
+    public void LoadPlayerSelectionScene()
+    {
+        SceneManager.LoadScene("PlayersSelectionScene");
+    }
+
+    public void LoadCreditsScene()
+    {
+        SceneManager.LoadScene("CreditsScene");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
